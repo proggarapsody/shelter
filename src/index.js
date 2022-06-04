e25c4e2b5b3664b13382de2178a357c8726ed893
Aleksey <76034634+proggarapsody@users.noreply.github.com>
2022-06-04 16:42:14 +0300

    feat: delete directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './styles/main.scss';
-import './js/shared.js';
-import './js/pets-slider.js';
-import './js/pagination.js';
-import './js/modal.js';
-import './js/nav.js';
-import './js/burger.js';
--- src/js/nav.js
+++
@@ -1,9 +0,0 @@
-import { closeNav } from './burger';
-
-const navItems = document.querySelectorAll('.nav-link');
-
-navItems.forEach((el) => {
-  el.addEventListener('click', () => {
-    closeNav();
-  });
-});
